1a1c1a77f96bb2d457a7a8c1852070be4c5412c9
donut <omar123yousef@gmail.com>
2022-01-30 17:37:27 -0600

    homework1

--- src/main/scala/homework1.scala
+++ src/main/scala/homework1.scala
@@ -1,3 +1,15 @@
-class homework1 {
+object homework1
+  enum expressions:
+    case Value(input: Any)
+    case Variable(name: String)
+    case Insert(name: String, op1: Int, op2: String)
+    case Assign(name: String, op: Insert)
 
-}
+    def operations: String =
+      this match {
+        case Variable(name) => name
+      }
+
+  @main def test: Unit =
+    println("Hello")
+
