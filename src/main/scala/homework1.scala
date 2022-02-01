bb00aeef5464c5b16118ccfd42c6f116c552c5bd
donut <omar123yousef@gmail.com>
2022-02-01 12:51:35 -0600

    homework1

--- src/main/scala/homework1.scala
+++ src/main/scala/homework1.scala
@@ -1,22 +1,28 @@
 
 object homework1:
-  private type typ = String | Int
+  private type typ = String | Int | Boolean
   enum expressions:
     case Value(input: typ)
     case Variable(name: String)
-    case Insert(name: Int, op1: typ, op2: typ)
-    case Assign(name: String, op: Set[typ])
-    private val map: Map[String, Set[typ]] = Map()
+    case Insert(name: expressions, op1: expressions, op2: expressions)
+    case Assign(name: expressions, op: expressions)
+    case Print(op: String)
+    case Check(name: String, value: expressions)
+    private val map: Map[String,Set[typ]] = Map()
 
     def eval: typ | Set[typ] =
       this match {
+        case Variable(op) => op
         case Value(op) => op
-        case Insert(op1, op2, op3) => Set(op1, op2, op3)
-        case Assign(op1, op2) => map(op1, op2.eval)
+        case Insert(op1, op2, op3) => Set(op1.eval, op2.eval, op3.eval)
+        case Assign(op1, op2) => map(op1.eval).union(op2.eval)
+        case Print(op1) => map(op1) //!! Temporary.. Delete later
+        case Check(op1, op2) => map.exists(op1).contains(op2)
       }
 
   @main def test(): Unit =
     import expressions.*
+    Assign(Variable("someSetName"), Insert(Variable("var"), Value(1), Value("somestring")))
 
-    println(Value("String").eval)
+    println(Print("someSetName"))
 
